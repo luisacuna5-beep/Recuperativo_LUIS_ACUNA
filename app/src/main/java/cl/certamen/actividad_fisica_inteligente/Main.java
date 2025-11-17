e85118957d69091e2a542428f5dcdb93f2146f56
Alumnos <fabian.reyes1@virginiogomez.cl>
2025-11-17 17:22:26 -0300

    Primer y ultimo commit

--- app/src/main/java/cl/certamen/actividad_fisica_inteligente/Main.java
+++ app/src/main/java/cl/certamen/actividad_fisica_inteligente/Main.java
@@ -1,6 +1,8 @@
 package cl.certamen.actividad_fisica_inteligente;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -30,8 +33,8 @@ public class Main extends AppCompatActivity {
     List<Sensor> listSensor;
     FusedLocationProviderClient fusedLocationProviderClient;
     DatabaseReference databaseReference;
-    double  latitudActual = 0.0;
-    double
+    double latitudActual = 0.0;
+    double longitudActual = 0.0;
 
 
     @Override
@@ -39,6 +42,12 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TVlatitud = findViewById(R.id.tvLatitud);
+        TVlongitud = findViewById(R.id.tvLongitud);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Ubicaciones");
+        obtener
 
         TVsensores= findViewById(R.id.tvSensores);
         BTiniciar=findViewById(R.id.btInicioReg);
@@ -58,6 +67,27 @@ public class Main extends AppCompatActivity {
         });
 
 
+    }
+    private void obtenerUbicacion(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new  String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE_LOCATION);
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this,location -> {
+            if (location != null){
+                latitudActual=location.getLatitude();
+                longitudActual=location.getLongitude();
+                TVlatitud.setText("latitud: " + latitudActual);
+                TVlongitud.setText("longitud: "+ longitudActual);
+
+
+            }
+            else {
+                TVlatitud.setText("Latitud: No disponible");
+                TVlongitud.setText("Longitud: No disponible");
+            }
+        });
     }
     private void verificarSensor()
     {
